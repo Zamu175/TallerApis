a78b3b51d12cd10b97c1248bdeb0f312926f55fd
SAMUEL samuelgutimuñozSsa <samuelgutierrezm@sasahotmail.com>
2018-10-15 18:05:03 -0500

    Versión final del taller

---
+++ TallerApis.Xamarin/TallerApis.Xamarin/Views/InicioPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TallerApis.Xamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InicioPage : ContentPage
+    {
+        public InicioPage()
+        {
+            InitializeComponent();
+        }
+        private async void Continuar(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PublicacionPage());
+        }
+    }
+}
---
+++ TallerApis.Xamarin/TallerApis.Xamarin/Views/PublicacionPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using TallerApis.Xamarin.Modelo;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TallerApis.Xamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PublicacionPage : ContentPage
+    {
+        public PublicacionPage()
+        {
+            CargarPublicaciones();
+        }
+
+        private void CargarPublicaciones()
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://192.168.0.11");
+            var request = client.GetAsync("/MiSocialApi/api/Publicacion").Result;
+            if (request.IsSuccessStatusCode)
+            {
+                var responseJson = request.Content.ReadAsStringAsync().Result;
+                var response = JsonConvert.DeserializeObject<List<Publicacion>>(responseJson);
+                listPublicaciones.ItemsSource = response;
+            }
+        }
+    }
+}
